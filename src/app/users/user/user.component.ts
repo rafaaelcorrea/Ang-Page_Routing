62f001e89b85cb8cd294d0fae30d0f0b32b07133
Rafael Correa <lumia.1986@outlook.com>
2023-05-12 15:05:14 -0300

    part 2 of Routing Page...

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute , Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -6,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
 
-  constructor() { }
+  user: {id: number, name: string};
+  paramsSubscription: Subscription;
+
+  constructor( private route: ActivatedRoute ) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+
+    /*using method "subscribe" ( observables) for change references in path in URL and loading correct datas
+    */
+   this.paramsSubscription =  this.route.params
+   .subscribe( 
+      ( params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
   }
 
 }
